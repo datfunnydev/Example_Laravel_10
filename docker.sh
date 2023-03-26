18040350a880f42d36c664d8f6aa003d33f4ddab
Dat <nguyentiendat@funnydev.vn>
2023-03-26 09:52:31 +0700

    Update

--- docker.sh
+++
@@ -1,7 +0,0 @@
-docker-compose down
-docker system prune -a
-docker build ./docker
-docker-compose up -d
-#docker-compose exec webserver bash #connect webserver terminal
-#docker exec -it mysql_80 mysql -uroot -p #connect database terminal
-#docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mysql_80 #get ip database
